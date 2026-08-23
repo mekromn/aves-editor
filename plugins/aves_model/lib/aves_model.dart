b78cd90ccb77812438afbf9597a8d87cfa8ead0b
mekromn <67240529+mekromn@users.noreply.github.com>
2026-08-23 01:14:24 -0700

    feat(editor): export edit recipe model

--- plugins/aves_model/lib/aves_model.dart
+++ plugins/aves_model/lib/aves_model.dart
@@ -9,6 +9,7 @@ export 'src/actions/move_type.dart';
 export 'src/actions/settings.dart';
 export 'src/actions/share.dart';
 export 'src/actions/slideshow.dart';
+export 'src/editor/edit_recipe.dart';
 export 'src/editor/enums.dart';
 export 'src/entry/base.dart';
 export 'src/events/actions.dart';
